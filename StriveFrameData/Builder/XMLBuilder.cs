8e985199f2ba7f8b268daf63488158329b880ddb
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2020-05-13 13:35:41 -0400

    Complete XML Export Logic

--- StriveFrameData/Builder/XMLBuilder.cs
+++ StriveFrameData/Builder/XMLBuilder.cs
@@ -21,31 +21,51 @@ namespace StriveFrameData.Builder
         {
             if (list == null || !list.Any() || string.IsNullOrEmpty(path)) return;
 
-            // todo finish creating all XElements 
-            // Hierarchy 
-            // 1. MainFrameData (Simple parent tag name) 
-            //   A. UI Tab Page Name i.e. tabMayPage and tabSolPage
-            //      i. Type of move i.e. StandingClose, StandingFar, Crouching groups
-            //          B. All of the UI elements that represent those types
-            //             i.e. StandingClosePunch and CrouchingKick
-
             bool workComplete = false;
             try
             {
+                // Hierarchy 
+                // 1. MainFrameData (Simple parent tag name) 
+                //   A. UI Tab Page Name i.e. tabMayPage and tabSolPage
+                //      i. Type of move i.e. StandingClose, StandingFar, Crouching groups.
+                //          B. All of the UI elements that represent those types
+                //             i.e. StandingClosePunch and CrouchingKick
+
                 XElement element =
                     new XElement("MainFrameData",
                         from po in list
                         select new XElement(po.TabPageName,
                             new XElement("StandingFarMoves",
                                 new XElement("StandingFarPunch", po.StandingFarPunch),
-                                new XElement("StandingFarKick", po.StandingFarKick))));
+                                new XElement("StandingFarKick", po.StandingFarKick),
+                                new XElement("StandingFarSlash", po.StandingFarSlash),
+                                new XElement("StandingFarHeavySlash", po.StandingFarHeavySlash),
+                                new XElement("StandingFarDust", po.StandingFarDust),
+                                new XElement("StandingFarNotApplicable", po.StandingFarNotApplicable)),
+                            new XElement("StandingCloseMoves",
+                                new XElement("StandingClosePunch", po.StandingClosePunch),
+                                new XElement("StandingCloseKick", po.StandingCloseKick),
+                                new XElement("StandingCloseSlash", po.StandingCloseSlash),
+                                new XElement("StandingCloseHeavySlash", po.StandingCloseHeavySlash),
+                                new XElement("StandingCloseDust", po.StandingCloseDust),
+                                new XElement("StandingCloseNotApplicable", po.StandingCloseNotApplicable)),
+                            new XElement("CrouchingMoves",
+                                new XElement("CrouchingPunch", po.CrouchingPunch),
+                                new XElement("CrouchingSlash", po.CrouchingSlash),
+                                new XElement("CrouchingHeavySlash", po.CrouchingHeavySlash),
+                                new XElement("CrouchingDust", po.CrouchingDust),
+                                new XElement("CrouchingNotApplicable", po.CrouchingNotApplicable)),
+                            new XElement("AdditionalNotes",
+                                new XElement("AdditionalNote", po.AdditionalNotesTextBoxText)),
+                            new XElement("ImportExportLocations",
+                                new XElement("ImportExportLocation", po.ImportExportLocationText))));
 
                 // Write complete XML element as XML page to file.
                 using (StreamWriter sw = new StreamWriter(path))
                 {
                     sw.WriteLine(element);
                 }
-
+                   
                 workComplete = true;
             }
             catch (Exception e)
