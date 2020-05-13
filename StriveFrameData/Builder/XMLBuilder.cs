5adc12cd73b39e0e03052efb66d858bf90a54ed4
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2020-05-13 12:13:39 -0400

    summary change

--- StriveFrameData/Builder/XMLBuilder.cs
+++ StriveFrameData/Builder/XMLBuilder.cs
@@ -22,11 +22,11 @@ namespace StriveFrameData.Builder
 
             // todo finish creating all XElements 
             // Hierarchy 
-            // 1. MainFrameData
-            //   A. UI Tab Page Name
+            // 1. MainFrameData (Simple parent tag name) 
+            //   A. UI Tab Page Name i.e. tabMayPage and tabSolPage
             //      i. Type of move i.e. StandingClose, StandingFar, Crouching groups
             //          B. All of the UI elements that represent those types
-            //             i.e. StandingClosePunch and Crouching Kick
+            //             i.e. StandingClosePunch and CrouchingKick
 
             XElement element =
                 new XElement("MainFrameData",
