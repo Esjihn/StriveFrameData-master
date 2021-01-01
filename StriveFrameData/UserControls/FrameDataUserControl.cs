db19415222521d1c7761a6fbc7a218155afc26b4
Matthew Miller <sysnom@hotmail.com>
2020-12-31 21:27:34 -0500

    refactor

--- StriveFrameData/UserControls/FrameDataUserControl.cs
+++ StriveFrameData/UserControls/FrameDataUserControl.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Drawing;
-using System.IO;
-using System.Linq;
 using System.Windows.Forms;
 using System.Xml.Linq;
 using StriveFrameData.Constants;
